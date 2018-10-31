25b4549017ef02971935bdf2234e7060927d9640
Şahin Alçın <36476466+sahinalcin@users.noreply.github.com>
2018-10-31 08:21:34 +0300

    Update Database.js

--- HW2/Database.js
+++ HW2/Database.js
@@ -42,13 +42,13 @@ addStudents(txt){
     let x=txt.split("\n");
 		m+=x.length;
     for(let line of x){
-		let y=line.split("\t");
-		let id=y[0],name=y[1],gpa=y[2];
-		let courses=[];
-		for(let i=3;i<y.length;i++){ 
-			courses.push(y[i]);}
-		let s=new Student(id,name,gpa,courses);
-		this.students.set(s.id,s);}      
+	let y=line.split("\t");
+	let id=y[0],name=y[1],gpa=y[2];
+	let courses=[];
+	for(let i=3;i<y.length;i++){ 
+		courses.push(y[i]);}
+	let s=new Student(id,name,gpa,courses);
+	this.students.set(s.id,s);}      
 }
 addCourses(txt){
     let m=txt.length;
@@ -60,52 +60,52 @@ addCourses(txt){
         let rooms=[];
         for(let i=3;i<y.length;i++){
             rooms.push(y[i]);}
-		let c=new Course(id,time,date,rooms);
-		this.courses.set(c.id,c);}
+	let c=new Course(id,time,date,rooms);
+	this.courses.set(c.id,c);}
 }
 clearOutput(){
 	out.innerText="";
 	return out.innerText;
 }
 randomStudent(){
-    let k=Array.from(this.students.keys())
-    let s=this.students.get(k[Math.trunc(k.length * Math.random())]);
+	let k=Array.from(this.students.keys())
+	let s=this.students.get(k[Math.trunc(k.length * Math.random())]);
 	out.innerText+="\n"+(s.id +" "+ s.name +" "+ s.gpa + " GPA");
-    return out.innerText;
+	return out.innerText;
 }
 aboveGPA(gpa){
-    let count=0;
-    for(let s of this.students.values())
-        if(s.gpa>gpa)
-			count+=1;
+	let count=0;
+	for(let s of this.students.values())
+        	if(s.gpa>gpa)
+		count+=1;
 	out.innerText="NUMBER OF THE STUDENTS ABOVE "+gpa+" GPA: "+count;
 	return out.innerText;
 }
 takenCourses(id){
-    let s=this.students.get(id);
+	let s=this.students.get(id);
 	out.innerText="\n\n"+(s.id+" "+s.name+" COURSES:\n\n"+s.courses);
 	return out.innerText;
 }
 examSchedules(id){
-    let s=this.students.get(id);
-    let schedule=s.id+" "+s.name+" EXAM SCHEDULE:\n";
+	let s=this.students.get(id);
+	let schedule=s.id+" "+s.name+" EXAM SCHEDULE:\n";
 	for(let x of s.courses){
-        let y = this.courses.get(x)
+        	let y = this.courses.get(x)
 		schedule+="\n" +y.id+" / "+y.date+" / "+y.time+" / "+y.rooms;}
 	out.innerText="\n\n"+schedule;
 	return out.innerText;
 }
 courseStudents(id){
-    let cs=id+" COURSE STUDENTS:\n";
-    for(let x of this.students.values()) {
-    	for(let y of x.courses){ 
+	let cs=id+" COURSE STUDENTS:\n";
+	for(let x of this.students.values()) {
+    		for(let y of x.courses){ 
 			if(y==id){cs+="\n"+x.id+" "+x.name;}}}
 	out.innerText="\n\n"+cs;
 	return out.innerText;
 }
 examroomCourses(id){
 	let erCourses=id+" EXAM ROOM COURSE LIST:\n";
-    for(let x of this.courses.values()) {
+	for(let x of this.courses.values()) {
 		for(let y of x.rooms){ 
 			if(y==id){erCourses+="\n"+x.id;}}}
 	out.innerText="\n\n"+erCourses			
@@ -113,7 +113,7 @@ examroomCourses(id){
 }
 examroomCount(id){
 	let count=0;
-    for(let x of this.courses.values()) {
+	for(let x of this.courses.values()) {
 		for(let y of x.rooms){ 
 			if(y==id){count+=1;}}}
 	out.innerText="NUMBER OF THE EXAM ROOM COURSES: " + count;
